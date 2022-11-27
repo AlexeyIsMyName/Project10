e98687564bc79e54d9eeff0c857c82f300efcb33
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-11-27 14:25:38 +0300

    make custom UICollectionViewCell class

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by ALEKSEY SUSLOV on 27.11.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
