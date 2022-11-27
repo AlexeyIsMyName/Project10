0825e1cdef5da7e0e06dcd31a8667f69cf3af39f
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-11-27 13:59:12 +0300

    Initial Commit

---
+++ Project10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by ALEKSEY SUSLOV on 27.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
